82d40763ce6ebea77d6099f15c20a8c162f8dba5
MrProperixaOriginal <95384465+MrProperixaOriginal@users.noreply.github.com>
2024-05-23 11:22:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import json
 import time
 
 api_link = "https://official-joke-api.appspot.com/random_joke"
-response = requests.get(api_link)
-if response.status_code == 200:
-    print(json.loads(response.text)["setup"])
-    time.sleep(2)
-    print(json.loads(response.text)["punchline"])
-else:
-    print(response.status_code)
+n = input("Сколько анекдотов вы хотите послушать?")
+for i in range(n):
+    response = requests.get(api_link)
+    if response.status_code == 200:
+        print(json.loads(response.text)["setup"])
+        time.sleep(2)
+        print(json.loads(response.text)["punchline"])
+    else:
+        print(response.status_code)
